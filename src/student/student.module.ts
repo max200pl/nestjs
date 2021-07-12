ff64182305ce75c79550367d33f8e642623e0446
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-12 10:46:53 +0300

    added middleware validation id student

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,10 +1,27 @@
 import { StudentController } from "./student.controller";
-import { Module } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { StudentService } from "./student.service";
+import { ValidStudentMiddleware } from "src/common/middleware/validStudent.middleware";
 
 @Module({
   providers: [StudentService],
   controllers: [StudentController],
   exports: [StudentService],
 })
-export class StudentsModule {}
+export class StudentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: "students/:studentId",
+      method: RequestMethod.GET,
+    });
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: "students/:studentId",
+      method: RequestMethod.PUT,
+    });
+  }
+}
